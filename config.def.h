38444925553613153465d01c2dcde2055fbf500e
Paul Gerald Pare <10038315-Sheape_lab@users.noreply.gitlab.com>
2023-07-06 18:42:10 +0800

    config

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=15:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -27,7 +27,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.25;
+static float chscale = 1.2;
 
 /*
  * word delimiter string
@@ -193,8 +193,8 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_KP_Plus,     zoom,           {.f = +1} },
-	{ TERMMOD,              XK_KP_Minus,    zoom,           {.f = -1} },
+	{ TERMMOD,              XK_E,           zoom,           {.f = +1} },
+	{ TERMMOD,              XK_N,           zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD|ShiftMask,    XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD|ShiftMask,    XK_V,           clippaste,      {.i =  0} },
